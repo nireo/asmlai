c1e0bdc3b0b01bdd560e4ef1957d4d955fd0071f
nireo <eemil.lehtikangas1@gmail.com>
2021-11-03 17:23:07 +0200

    fixed multiple parameters

--- out.s
+++ out.s
@@ -6,7 +6,12 @@ xd:
 	pushq	%rbp
 	movq	%rsp, %rbp
 	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
 	addq	$-16, %rsp
+	movslq	-8(%rbp), %r10
+	movq	%r10, %rdi
+	call	print_num
+	movq	%rax, %r10
 	movslq	-4(%rbp), %r10
 	movq	%r10, %rdi
 	call	print_num
@@ -24,6 +29,8 @@ main:
 	addq	$0, %rsp
 	movq	$100, %r10
 	movq	%r10, %rdi
+	movq	$10, %r10
+	movq	%r10, %rsi
 	call	xd
 	movq	%rax, %r10
 	movq	$0, %r10
