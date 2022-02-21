e4f4a5ff64d1571cf3a486e2d7dde4cbde00c2f2
nireo <eemil.lehtikangas1@gmail.com>
2022-02-21 21:10:35 +0200

    added a basic code generator

---
+++ codegen.cc
@@ -0,0 +1,88 @@
+#include "codegen.h"
+#include <cassert>
+#include <cstdio>
+
+namespace codegen {
+static i64 depth{};
+
+static void push() {
+  printf("  push %%rax\n");
+  ++depth;
+}
+
+static void pop(const char *argument) {
+  printf("  pop %s\n", argument);
+  --depth;
+}
+
+static void gen_expression(const parser::Node &node) {
+  using NodeType = parser::NodeType;
+  if (node.type_ == NodeType::Num) {
+    printf("  mov $%ld, %%rax\n", std::get<i64>(node.data_));
+    return;
+  } else if (node.type_ == NodeType::Neg) {
+    gen_expression(*node.lhs_);
+    printf("  neg %%rax\n");
+    return;
+  }
+
+  gen_expression(*node.rhs_);
+  push();
+
+  gen_expression(*node.lhs_);
+  pop("%rdi");
+
+  switch (node.type_) {
+  case NodeType::Add: {
+    printf("  add %%rdi, %%rax\n");
+    return;
+  }
+  case NodeType::Sub: {
+    printf("  sub %%rdi, %%rax\n");
+    return;
+  }
+  case NodeType::Mul: {
+    printf("  imul %%rdi, %%rax\n");
+    return;
+  }
+  case NodeType::Div: {
+    printf("  cqo\n");
+    printf("  idiv %%rdi\n");
+    return;
+  }
+
+  case NodeType::EQ:
+  case NodeType::LT:
+  case NodeType::NE:
+  case NodeType::LE: {
+    printf("  cmp %%rdi, %%rax\n");
+
+    if (node.type_ == NodeType::EQ) {
+      printf("  sete %%al\n");
+    } else if (node.type_ == NodeType::NE) {
+      printf("  setne %%al\n");
+    } else if (node.type_ == NodeType::LT) {
+      printf("  setl %%al\n");
+    } else if (node.type_ == NodeType::LE) {
+      printf("  setle %%al\n");
+    }
+
+    printf("  movzb %%al, %%rax\n");
+    return;
+  }
+  default: {
+    std::fprintf(stderr, "invalid node type\n");
+  }
+  }
+}
+
+void gen_code(parser::NodePtr root) {
+  printf("  .globl main\n");
+  printf("main:\n");
+
+  gen_expression(*root);
+
+  printf("  ret\n");
+  assert(depth == 0);
+}
+} // namespace codegen
---
+++ codegen.h
@@ -0,0 +1,10 @@
+#ifndef _ASMLAI_CODEGEN_H
+#define _ASMLAI_CODEGEN_H
+
+#include "parser.h"
+
+namespace codegen {
+void gen_code(parser::NodePtr nodes);
+};
+
+#endif
